a7d6f1cd2ac4242cb931dcd141ddd30be63bcd65
Sam3570 <sameersayed3570@gmail.com>
2025-09-17 17:28:43 +0530

    CORS-issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      frontendApi={process.env.CLERK_FRONTEND_API}>
       <html lang="en">
         <body cz-shortcut-listen="true">
           <AuthWrapper>
