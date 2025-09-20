2e3c0934c73a7a7e5cd9d328e56de84d9acb12e7
Sam3570 <sameersayed3570@gmail.com>
2025-09-21 01:37:33 +0530

    check clerk-3

--- src/aws-config.ts
+++ src/aws-config.ts
@@ -1,6 +1,9 @@
 // aws.config.ts
 import { S3Client } from "@aws-sdk/client-s3";
 
+console.log("s3",process.env.MY_AWS_ACCESS_KEY!)
+console.log("s3",process.env.MY_AWS_SECRET_KEY!)
+
 export const s3 = new S3Client({
   region: process.env.MY_AWS_REGION!,
   credentials: {
